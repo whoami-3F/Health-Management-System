c5b8c840d0bd4865c14671a7483d29800634c5f8
whoami-3F <saurav.stamang@gmail.com>
2024-05-24 04:09:27 +0545

    modifying app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -7,8 +9,9 @@ function App() {
   return (
     <>
       <div>
-      <h1>Working on this Project</h1>
-    </>
+        <h1>Working on this project</h1>
+        </div>
+          </>
   )
 }
 
