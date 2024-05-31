5b0541190d37f045adab1152a3499126c531b035
whoami-3F <saurav.stamang@gmail.com>
2024-05-31 07:01:56 +0545

    adding header and footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, 
+  createBrowserRouter, createRoutesFromElements 
+} from 'react-router-dom'
+import Home from './Components/Home/Home'
+import Layout from "./Layout.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
